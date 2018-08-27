12000717f02414b79dbbd9f6f975799fdadf022a
Vicky Coronel <victoria.coronel.224@gmail.com>
2018-08-27 15:31:20 -0500

    Registro de la URL para Usuario

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,24 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+/*el navegador chrome solo admite  GET 
+POR LO QUE UTILIZAREMOS POSTMAN ,ahí hay que seleccionar Body-raw-JSON y enviar
+los campos con sus respectivos datos en formato JSON para editar un registro.
+ * get se utiliza más para  listar o buscar*/
+
+
+////EndPoints de USUARIO
+$router->get('/usuario/listar', 'ControladorUsuario@listarUsuarios');//
+$router->post('/usuario/modificar/{external_id}', 'ControladorUsuario@modificarUsuario');
+$router->post('/usuario/login', 'ControladorUsuario@inicioSesion');
+/*seleccionar Body-raw-JSON y enviar el correo y clave en formato json 
+ * para loguearse, y como extra obtendremos el token
+ */
+$router->post('/usuario/buscar/{external_id}', 'ControladorUsuario@buscarUsuario');
+$router->post('/usuario/registrar', 'ControladorUsuario@registrarUsuario');
+
+
